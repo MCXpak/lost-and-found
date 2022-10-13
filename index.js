43edc0568137b5dc557c68ceaef242a2d0c4e4c0
fsaiz97 <felixsaiz97@gmail.com>
2022-10-13 15:30:27 +0100

    Update hamburger menu event listener to work on pressing Enter

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector("#hamburger-menu");
 
-hamburger.addEventListener("click", (e) => {
+const toggleNav = () => {
+
     const nav = document.querySelector("#navigation");
     const displayType = (nav.style.display === "none") ? "flex" : "none";
     nav.style.display = displayType;
-})
+}
+
+const toggleNavWrapper = (e) => {
+    if ((e.type === "keydown" && e.code === "Enter") || e.type === "click") {
+        toggleNav();
+    }
+}
+
+hamburger.addEventListener("click", toggleNavWrapper);
+hamburger.addEventListener("keydown", toggleNavWrapper);
+
